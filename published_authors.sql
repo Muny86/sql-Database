eacb38d63478c4224b76576ccf5929f6cb7983bc
Munir <muniryousef86@gmail.com>
2019-03-08 09:39:11 -0500

    Add files via upload

---
+++ published_authors.sql
@@ -0,0 +1,127 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.8.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Mar 08, 2019 at 02:00 PM
+-- Server version: 5.7.24-log
+-- PHP Version: 7.2.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `published_authors`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `best_selling_books`
+--
+
+CREATE TABLE `best_selling_books` (
+  `id` varchar(50) DEFAULT NULL,
+  `book_name` text,
+  `book_genre` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `best_selling_books`
+--
+
+INSERT INTO `best_selling_books` (`id`, `book_name`, `book_genre`) VALUES
+('27-0979655', 'Etiam faucibus cursus urna. Ut tellus. Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi. Cras non velit nec nisi vulputate nonummy. Maecenas tincidunt lacus at velit. Vivamus vel nulla eget eros elementum pellentesque. Quisque porta volutpat erat. Quisque erat eros, viverra eget, congue eget, semper rutrum, nulla.', 'Comedy'),
+('24-8285518', 'Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo. In blandit ultrices enim. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Proin interdum mauris non ligula pellentesque ultrices.', 'Drama'),
+('28-3417259', 'In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum. Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo placerat. Praesent blandit. Nam nulla. Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.', 'Drama|Romance'),
+('18-6525280', 'Donec semper sapien a libero. Nam dui.', 'Drama'),
+('18-7936531', 'Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vivamus vestibulum sagittis sapien. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Etiam vel augue. Vestibulum rutrum rutrum neque. Aenean auctor gravida sem. Praesent id massa id nisl venenatis lacinia. Aenean sit amet justo. Morbi ut odio. Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.', 'Drama|Romance'),
+('20-9940420', 'Nulla nisl. Nunc nisl. Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa. Donec dapibus. Duis at velit eu est congue elementum. In hac habitasse platea dictumst. Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante. Nulla justo.', 'Comedy|Drama|Fantasy'),
+('68-2517103', 'Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Mystery|Thriller'),
+('22-8229674', 'Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis. Nam congue, risus semper porta volutpat, quam pede lobortis ligula, sit amet eleifend pede libero quis orci. Nullam molestie nibh in lectus. Pellentesque at nulla.', 'Animation'),
+('91-6899493', 'Morbi quis tortor id nulla ultrices aliquet. Maecenas leo odio, condimentum id, luctus nec, molestie sed, justo. Pellentesque viverra pede ac diam. Cras pellentesque volutpat dui. Maecenas tristique, est et tempus semper, est quam pharetra magna, ac consequat metus sapien ut nunc. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam. Suspendisse potenti.', 'Comedy|Crime|Drama|Mystery'),
+('08-6381971', 'Pellentesque at nulla. Suspendisse potenti. Cras in purus eu magna vulputate luctus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.', 'Drama'),
+('27-3803592', 'Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum. Curabitur in libero ut massa volutpat convallis. Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.', 'Drama|War'),
+('19-8475867', 'In hac habitasse platea dictumst. Maecenas ut massa quis augue luctus tincidunt. Nulla mollis molestie lorem. Quisque ut erat. Curabitur gravida nisi at nibh. In hac habitasse platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer eget, rutrum at, lorem. Integer tincidunt ante vel ipsum.', 'Action|Adventure|Animation|Children|Comedy|Sci-Fi'),
+('28-7111791', 'Donec semper sapien a libero. Nam dui. Proin leo odio, porttitor id, consequat in, consequat ut, nulla. Sed accumsan felis.', 'Comedy|Musical|Romance'),
+('06-9706276', 'Morbi non lectus. Aliquam sit amet diam in magna bibendum imperdiet. Nullam orci pede, venenatis non, sodales sed, tincidunt eu, felis. Fusce posuere felis sed lacus. Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl. Nunc rhoncus dui vel sem. Sed sagittis.', 'Documentary'),
+('95-9494511', 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo. Maecenas pulvinar lobortis est. Phasellus sit amet erat. Nulla tempus. Vivamus in felis eu sapien cursus vestibulum. Proin eu mi. Nulla ac enim. In tempor, turpis nec euismod scelerisque, quam turpis adipiscing lorem, vitae mattis nibh ligula nec sem. Duis aliquam convallis nunc.', 'Drama|Fantasy|Mystery|Sci-Fi');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `publishers`
+--
+
+CREATE TABLE `publishers` (
+  `id` varchar(50) DEFAULT NULL,
+  `publisher` varchar(50) DEFAULT NULL,
+  `hq_location` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `publishers`
+--
+
+INSERT INTO `publishers` (`id`, `publisher`, `hq_location`) VALUES
+('38-0982913', 'Geba', 'Karangbalong'),
+('69-1558279', 'Twitterlist', 'Houping'),
+('76-7923926', 'Yodel', 'Cidahu'),
+('44-8885512', 'Devbug', 'Mafeng'),
+('63-0924452', 'Yambee', 'Santaluz'),
+('04-4676592', 'Browsedrive', 'Plan de Ayala'),
+('54-5412414', 'Eamia', 'Viga'),
+('35-7812203', 'Kwimbee', 'Qitai'),
+('69-5437358', 'Rhyloo', 'Hali'),
+('15-0632685', 'Skipstorm', 'Cikulina'),
+('29-1645107', 'Tavu', 'Kedungsumurkrajan'),
+('67-5538936', 'Aivee', 'Oriximiná'),
+('78-7337774', 'Voonix', 'Opařany'),
+('47-5793402', 'Omba', 'Jōetsu'),
+('73-5365044', 'Tekfly', 'Daliuhang');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `writer`
+--
+
+CREATE TABLE `writer` (
+  `id` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) DEFAULT NULL,
+  `best_seller` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `writer`
+--
+
+INSERT INTO `writer` (`id`, `first_name`, `best_seller`) VALUES
+('03-1287824', 'Wileen', '0'),
+('91-8312225', 'Zaccaria', '1'),
+('84-0718625', 'Kaia', '0'),
+('97-7980670', 'Dora', '0'),
+('41-9155881', 'Lacey', '1'),
+('14-8519485', 'Janel', '1'),
+('24-0024806', 'Marco', '1'),
+('82-3355308', 'Kalvin', '0'),
+('81-6323222', 'Zach', '0'),
+('17-8300092', 'Layton', '0'),
+('36-0189153', 'Katharine', '0'),
+('88-3200210', 'Hamlin', '0'),
+('23-5969964', 'Merry', '1'),
+('19-2128855', 'Rutherford', '1'),
+('65-8366256', 'Rhona', '1');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
